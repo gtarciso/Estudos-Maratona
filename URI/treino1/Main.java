476ce0bdfb7712abaf8909d1bdedd0f82db91663
gtarciso <gustavotarciso@hotmail.com>
2018-05-14 14:40:09 -0300

    1380 aceito

--- URI/treino1/Main.java
+++ URI/treino1/Main.java
@@ -4,41 +4,56 @@ import java.util.Scanner;
  
 
 /*
- * Problema número 1380 do URI - não aceito (time limit exceeded)
+ * Problema número 1380 do URI
  * Problema I do treino 1
  */
 
 public class Main {
+
+    public static final BigInteger ONE = new BigInteger("1");
+    public static final BigInteger ZERO = new BigInteger("0");
  
     public static void main(String[] args) throws IOException {
-    	int values[] = new int[1500];
-    	int pd[] = new int [1500];
+    	BigInteger values[] = new BigInteger[1500];
+    	int pd[] = new int [1501];
+        values[0] = ZERO;
+        values[1] = ONE;
+        BigInteger mil = new BigInteger("1000");
  
         Scanner io = new Scanner(System.in);
-        int n = io.nextInt();
-        for(int i = 0; i < n; i++) {
+        String n = io.next();
+        BigInteger nth = new BigInteger(n);
+        fib(values, 1499);
+        for(int i = 0; i < 1500; i++) {
+            values[i] = values[i].mod(mil);
+            pd[i] = values[i].intValue(); 
+        }
+        for(BigInteger i = ZERO; !i.equals(nth) ; i = i.add(ONE)) {
         	BigInteger number;
         	String aux = io.next();
-        	number = new BigInteger(aux);
-        	number = number.mod(new BigInteger("1500"));
-        	int x = number.intValue();
-        	int a;
-        	if(pd[x] == 1) {
-        		a = values[x];
-        	} else {
-        		pd[x] = 1;
-        		a = fib(x)%1000;
-        		values[x] = a;
-        	}
-        	if(x < 10) {
+            char x[] = aux.toCharArray();
+            int comp = aux.length();
+            int soma = 0;
+            for(int j = 0; j < comp; j++) {
+                soma = (soma*10 + x[j] - '0')%1500;
+            }
+        	int a = pd[soma];
+            
+        	if(a < 10) {
         		System.out.println("00"+a);
-        	} else if(x < 100) {
+        	} else if(a < 100) {
         		System.out.println("0"+a);
         	} else {
         		System.out.println(a);
         	}
         }
  
+    }
+
+    public static void fib(BigInteger c[], int n) {
+        for(int i = 2; i <= n; i++) {
+            c[i] = c[i-1].add(c[i-2]);
+        }
     }
 
     private static int fib(int n) {
